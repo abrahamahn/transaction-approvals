e80ad5863103f0d0725c3834c333e7d7fde56791
Abe Ahn <abemsq@gmail.com>
2023-05-10 05:07:21 +0900

    first commit

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -9,7 +9,7 @@ export function Instructions() {
         <p className="RampText--d">
           <h1 className="RampTextHeading--m">
             Great news!
-          </h1><br />I wanted to let you know that I've finished all the necessary bug fixes and added some extra touches to improve the overall look and feel of the project. <br /><br />I even made sure to align everything with Ramp's brand identity and took inspiration from their website to make some additional tweaks. If you have any questions or concerns, please don't hesitate to reach out to me.
+          </h1><br />I wanted to let you know that I've finished all the necessary bug fixes and added some extra touches to improve the overall look and feel of the project. Unfortunately, bug #7 was not addressed on the fix due to time constraints. <br /><br />However, I made sure that CSS aligns everything with Ramp's brand identity and took inspiration from the company website to make some additional tweaks. If you have any questions or concerns, please don't hesitate to reach out to me.
           <br/><br/>Abraham J. Ahn< br/>
           satmorningrain@gmail.com
         </p>
