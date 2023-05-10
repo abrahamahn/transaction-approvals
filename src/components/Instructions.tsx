9fc51ae0f103d1995cfd7c1f90a03bd30f81a4fa
Abe Ahn <abemsq@gmail.com>
2023-05-10 10:11:44 +0900

    add README.md

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -7,11 +7,9 @@ export function Instructions() {
       <div className="RampDiv">
         <div className="RampBreak--l" />
         <p className="RampText--d">
-          <h1 className="RampTextHeading--m">
-            Great news!
-          </h1><br />I wanted to let you know that I've finished all the necessary bug fixes and added some extra touches to improve the overall look and feel of the project. Unfortunately, bug #7 was not addressed on the fix due to time constraints. <br /><br />However, I made sure that CSS aligns everything with Ramp's brand identity and took inspiration from the company website to make some additional tweaks. If you have any questions or concerns, please don't hesitate to reach out to me.
-          <br/><br/>Abraham J. Ahn< br/>
-          satmorningrain@gmail.com
+          <span className="RampBreak--s" />
+          Select the checkbox on the right to approve or decline the transactions. You can filter
+          transactions by employee.
         </p>
       </div>
     </Fragment>
